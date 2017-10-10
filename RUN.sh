4f2ea47b26e26f195e234e9a4d06575cfcdac6fb
Johannes Fessler <johannes.fessler@bemindo.com>
2017-10-10 12:12:46 +0200

    modified RUN-script

--- RUN.sh
+++ RUN.sh
@@ -1,9 +1,19 @@
-x=`pwd`
+#prepare parameters and commands
+ip=`ifconfig wlo1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+path=`pwd`
+serverCmd="bash -c \"$path/runServer 5001 ./mailStorage\""
+clientCmd="bash -c \"$path/runClient 5001 $ip\""
+
+#compile both files
 gcc -Wall main.c -o runServer
 gcc -Wall client.c -o runClient
-serverCmd='bash -c \"$x/runServer 5001 ./mailStorage\"'
-echo $serverCmd
+
+#print information
+echo "ip = $ip"
+echo "path = $path"
+echo "start server: \"$serverCmd\""
+echo "start client: \"$clientCmd\""
+
+#run commands
 gnome-terminal -e "$serverCmd"
-clientCmd='bash -c \"$x/runClient 5001 192.168.0.178\"'
-echo $clientCmd
 gnome-terminal -e "$clientCmd"
