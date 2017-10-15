9447649e8cf0634913b7eb3e3aac67d377d403f2
Johannes Fessler <johannes.fessler@bemindo.com>
2017-10-15 23:53:35 +0200

    Server: listdir and searchUser finished, Client: sendMessage finished

--- RUN.sh
+++ RUN.sh
@@ -1,16 +1,18 @@
 #prepare parameters and commands
-ip=`ifconfig ens33 | grep 'inet Adresse:' | cut -d: -f2 | awk '{ print $1}'`
+ip=`ifconfig wlo1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 path=`pwd`
-serverCmd="bash -c \"Server/bin/Debug/Server 5001 ./mailStorage\""
-clientCmd="bash -c \"Client/bin/Debug/Client 5001 $ip\""
+port=$(shuf -i 5000-9999 -n 1)
+serverCmd="bash -c \"$path/runServer $port .\""
+clientCmd="bash -c \"$path/runClient $port $ip\""
 
 #compile both files
-gcc -Wall main.c -o runServer
-gcc -Wall client.c -o runClient
+g++ -Wall ./Server.cpp -o ./runServer
+g++ -Wall ./Client.cpp -o ./runClient
 
 #print information
 echo "ip = $ip"
 echo "path = $path"
+echo "Port = $port"
 echo "start server: \"$serverCmd\""
 echo "start client: \"$clientCmd\""
 
